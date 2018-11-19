e8b3ad0e8e4fda099630638e1cbfa4d7f13ad833
Semyon Fomin <taxnuke@gmail.com>
2018-11-19 23:26:09 +0300

    добавляет деплой на распи

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,27 +1,9 @@
 module.exports = {
-  // todo: Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+  // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
   apps: [
     {
       /**
-       * CLIENT
-       */
-      name: 'url-shortener-status-client',
-      script: 'source/client.js',
-      env: {
-        watch: true,
-        WS_PORT: 9897,
-        WS_HOST: 'localhost',
-        NODE_ENV: 'development'
-      },
-      env_production: {
-        WS_PORT: 9897,
-        WS_HOST: '138.68.183.160',
-        NODE_ENV: 'production'
-      }
-    },
-    {
-      /**
-       * SERVER
+       * СЕРВЕР
        */
       name: 'url-shortener-status-server',
       script: 'source/server.js',
@@ -31,23 +13,59 @@ module.exports = {
         WS_PORT: 9897,
         NODE_ENV: 'development'
       },
-      env_production: {
+      env_vps: {
         HOOK_PORT: 8813,
         WS_PORT: 9897,
+        NODE_ENV: 'production'
+      },
+      env_raspberry: {
+        HOOK_PORT: 8813,
+        WS_PORT: 9897,
+        NODE_ENV: 'production'
+      }
+    },
+    {
+      /**
+       * КЛИЕНТ
+       */
+      name: 'url-shortener-status-client',
+      script: 'source/client.js',
+      env: {
+        watch: true,
+        WS_PORT: 9897,
+        WS_HOST: 'localhost',
+        NODE_ENV: 'development'
+      },
+      env_vps: {
+        WS_PORT: 9897,
+        WS_HOST: '138.68.183.160',
+        NODE_ENV: 'production'
+      },
+      env_raspberry: {
+        WS_PORT: 9897,
+        WS_HOST: '138.68.183.160',
         NODE_ENV: 'production'
       }
     }
   ],
 
   deploy: {
-    // todo: надо найти способ задеплоить на Pi
-    production: {
+    vps: {
       user: 'adminus',
       host: '138.68.183.160',
       ref: 'origin/master',
       repo: 'git@github.com:taxnuke/url-shortener-status.git',
       path: '/var/www/stat.short.taxnuke.ru',
-      'post-deploy': 'npm i && pm2 reload ecosystem.config.js --env production'
+      'post-deploy': 'npm i && pm2 reload ecosystem.config.js --env vps --only url-shortener-status-server'
+    },
+    raspberry: {
+      user: 'pi',
+      // задеплоить на Raspberry можно только с локалки, соответственно
+      host: '192.168.1.70',
+      ref: 'origin/master',
+      repo: 'git@github.com:taxnuke/url-shortener-status.git',
+      path: '/var/www/stat.short.taxnuke.ru',
+      'post-deploy': 'npm i && pm2 reload ecosystem.config.js --env raspberry --only url-shortener-status-client'
     }
   }
 }
