d1417a83f2ceaea0291a68351c3d951f2f4d64a4
Semyon Fomin <taxnuke@gmail.com>
2018-11-19 23:26:28 +0300

    добавляет тестовый вывод на экран для дебага

--- source/client.js
+++ source/client.js
@@ -2,8 +2,8 @@
 const WebSocket = require('ws')
 
 // todo: пояснить магические числа
-// const lcdConnection = new I2CLCDConnection(1, 0x3f, 16, 2);
-// lcdConnection.println('short.taxnuke.ru', 1);
+const lcdConnection = new I2CLCDConnection(1, 0x3f, 16, 2);
+lcdConnection.println('short.taxnuke.ru', 1);
 
 const ws = new WebSocket(`ws://${process.env.WS_HOST}:${process.env.WS_PORT}`)
 
